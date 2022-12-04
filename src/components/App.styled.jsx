6c42234d0066cbcb9da54ef10c391950382f6893
VladosSid <sidorsv121097@gmail.com>
2022-12-04 14:21:31 +0200

    fixstyled app

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
 
-export const GlobalBox = styled.div`
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    color: '#010101',
-    `;
+export const GlobalBox = styled.ol`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #010101;
+`;
